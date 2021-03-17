c6285a18ccfca6e05364a4e451c0e66b54125ea9
qingbing <780042175@qq.com>
2021-03-17 10:46:58 +0800

    table组件添加查询使用ref

--- src/views/table/pagination.vue
+++ src/views/table/pagination.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
+    <el-form
+      :inline="true"
+      :model="queryForm"
+      class="demo-form-inline"
+      ref="query"
+    >
+      <el-form-item label="审批人">
+        <el-input v-model="queryForm.user" placeholder="审批人"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="queryForm.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
     <e-table
       :getHeaders="getHeaders"
       :getTableData="getData"
       :beforeRender="beforeRender"
       :pagination="pagination"
+      ref="pageTable"
     ></e-table>
     <div>
       {{ pagination }}
@@ -24,23 +44,31 @@ export default {
   },
   data() {
     return {
+      refreshFlag: 1,
       pagination: {
-        // pageNo: 2,
+        pageNo: 1,
         pageSize: 15,
       },
-      params: {
-        sex: "1",
+      queryForm: {
+        user: "1",
+        region: "",
       },
       beforeRender(item, idx) {},
     };
   },
   methods: {
+    onSubmit() {
+      console.log(this.$refs["pageTable"].refreshTable());
+    },
     getHeaders(cb) {
       ajaxMethod("/header-user", { type: "user" }, "post", (res) => cb(res));
     },
     getData(cb) {
-      ajaxMethod("/user-pagination", merge(this.pagination, this.params), "post", (res) =>
-        cb(res)
+      ajaxMethod(
+        "/user-pagination",
+        merge(this.pagination, this.queryForm),
+        "post",
+        (res) => cb(res)
       );
     },
   },
